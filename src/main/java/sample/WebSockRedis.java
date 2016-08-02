5f072d8aa534111edef5f918762632da255c509e
root <root@ip-172-31-39-203.us-west-2.compute.internal>
2016-08-02 12:26:51 +0000

    jedis connection change

--- src/main/java/sample/WebSockRedis.java
+++ src/main/java/sample/WebSockRedis.java
@@ -9,32 +9,63 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import redis.clients.jedis.Jedis;
+
 @ServerEndpoint(value = "/wsocket")
 public class WebSockRedis {
-    //notice:not thread-safe
+    // notice:not thread-safe
     public static ArrayList<Session> sessionList = new ArrayList<Session>();
-    
+
     @OnOpen
-    public void onOpen(Session session){
-        try{
+    public void onOpen(Session session) {
+        try {
             sessionList.add(session);
-            //asynchronous communication
+            // asynchronous communication
             session.getBasicRemote().sendText("Hello!");
-        }catch(IOException e){}
+            try {
+                final Jedis jedisInstance = new Jedis("ec2-52-33-201-37.us-west-2.compute.amazonaws.com", 6379, 120000);
+
+                final RedisKeyListener redisKeyListener = new RedisKeyListener();
+
+                // jedisInstance.psubscribe(redisKeyListener, "__key*__:foo*");
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            System.out.println("Subscribing to \"__key*__:user*\". This thread will be blocked.");
+                            // jedis1.psubscribe(keyExpiredListener,
+                            // "__key*__:*");
+                            jedisInstance.psubscribe(redisKeyListener, "__key*__:user*");
+                            System.out.println("Subscription ended.");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            System.out.println("Subscribing failed." + e);
+                        }
+                    }
+                }).start();
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch (IOException e) {
+        }
     }
-    
+
     @OnClose
-    public void onClose(Session session){
+    public void onClose(Session session) {
         sessionList.remove(session);
     }
-    
+
     @OnMessage
-    public void onMessage(String msg){
-        try{
-            for(Session session : sessionList){
-                //asynchronous communication
+    public void onMessage(String msg) {
+        try {
+            for (Session session : sessionList) {
+                // asynchronous communication
                 session.getBasicRemote().sendText(msg);
             }
-        }catch(IOException e){}
+        } catch (IOException e) {
+        }
     }
 }
